9cefbb99e81658a5c49504e5c48431bed90f2bd9
Jubair <jubair.ahmed2838@gmail.com>
2023-09-12 00:01:29 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from '../../assets/images/profile.png'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between border-b-2 pb-4 mt-8 items-center'>
+      <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>
+      <img className='w-18' src={Profile} alt="" />
+    </div>
+  )
+}
+
+export default Header
